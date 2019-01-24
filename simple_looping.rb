34a0e1a69435b1400f1a2fe65e0232cf6beb3777
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-24 21:10:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,8 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  until number_of_times == 7
+    puts phrase
 end
 
 def for_iterator(number_of_times)
