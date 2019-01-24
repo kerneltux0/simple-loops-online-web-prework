40197ac763ff3882904ee4976c62c90d6277dfe5
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-24 21:09:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,14 +39,17 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for number_of_times in 1..7 do
-
+    puts phrase
+  end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  for number_of_times in 1..7 do
+    puts phrase
+  end
   
 end
 
