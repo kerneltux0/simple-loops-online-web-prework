7b98f7c3caf56b10e4d07a50cd86256088ae97ed
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-24 21:05:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,7 +20,7 @@ end
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "times" keyword
-  loop do
+  10.times do
     puts phrase
   
 end
